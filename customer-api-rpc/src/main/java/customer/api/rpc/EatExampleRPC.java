a8fc1ee9fc1ca1e3265196b0abfd42e9c1e8d2ca
李楠 <linan.groot@outlook.com>
2020-12-30 10:32:45 +0800

    RPC：相关代码更新

---
+++ customer-api-rpc/src/main/java/customer/api/rpc/EatExampleRPC.java
@@ -0,0 +1,10 @@
+package customer.api.rpc;
+
+/**
+ * <p>Description: </p>
+ *
+ * @author linan
+ * @date 2020-12-29
+ */
+public interface EatExampleRPC {
+}
---
+++ customer-api-service/src/main/java/customer/api/service/implement/EatExampleServiceImp.java
@@ -0,0 +1,10 @@
+package customer.api.service.implement;
+
+/**
+ * <p>Description: </p>
+ *
+ * @author linan
+ * @date 2020-12-29
+ */
+public class EatExampleServiceImp {
+}
